64ed887cf32b198a738eef421bf438fee2d66849
Yara <yaralviana@gmail.com>
2024-06-15 12:01:41 -0300

    Modificando produtos

--- exercicios/para-sala/main.ts
+++ exercicios/para-sala/main.ts
@@ -1,12 +1,12 @@
 import { ProdutoInterface } from "./produtos/interfaces/produtos.interface"
 import { Produto } from "./produtos/produto"
 
-const produto = new Produto("Regata", 100, "descricao regata M")
+const produto = new Produto("Gaveteira com rodinhas", 250, "Gaveteiro para escritório com 4 gavetas")
 
 const produtoObject: ProdutoInterface = {
-nome: "produto obj",
-descricao: "",
-preco: 0
+nome: "Cadeira de escritório",
+descricao: "Assento estofado em PU oferece ergonomia e conforto",
+preco: 468
 }
 
 const display = (...produtos: ProdutoInterface[]): void => {
